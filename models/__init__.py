7c9f314f87a8de8355f9b0d7b8088df91a2da5a4
Bingchen Gong <r@s2.hk>
2022-11-19 19:40:24 +0800

    Code Style

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from models.colorRF import ColorVMSplit
 from models.tensoRF import TensorVM, TensorCP, TensorVMSplit
-from models.tensorBase import AlphaGridMask
 
-MODEL_ZOO = {a.__name__: a for a in (TensorVM, TensorCP, TensorVMSplit, AlphaGridMask, ColorVMSplit)}
+MODEL_ZOO = {a.__name__: a for a in (TensorVM, TensorCP, TensorVMSplit, ColorVMSplit)}
--- models/sh.py
+++ models/sh.py
@@ -12,16 +12,15 @@ C4 = (2.5033429417967046, -1.7701307697799304, 0.9461746957575601, -0.6690465435
 
 def eval_sh(deg, sh, dirs):
     """
-    Evaluate spherical harmonics at unit directions
-    using hardcoded SH polynomials.
+    Evaluate spherical harmonics at unit directions using hardcoded SH polynomials.
     Works with torch/np/jnp.
-    ... Can be 0 or more batch dimensions.
+    ... Can be 0 or more batch dimensions
     :param deg: int SH max degree. Currently, 0-4 supported
     :param sh: torch.Tensor SH coeffs (..., C, (max degree + 1) ** 2)
     :param dirs: torch.Tensor unit directions (..., 3)
     :return: (..., C)
     """
-    assert deg <= 4 and deg >= 0
+    assert 0 <= deg <= 4
     assert (deg + 1) ** 2 == sh.shape[-1]
     C = sh.shape[-2]
 
@@ -66,48 +65,44 @@ def eval_sh(deg, sh, dirs):
 
 def eval_sh_bases(deg, dirs):
     """
-    Evaluate spherical harmonics bases at unit directions,
-    without taking linear combination.
-    At each point, the final result may the be
-    obtained through simple multiplication.
+    Evaluate spherical harmonics bases at unit directions, without taking linear combination.
+    At each point, the final result may to be obtained through simple multiplication
     :param deg: int SH max degree. Currently, 0-4 supported
     :param dirs: torch.Tensor (..., 3) unit directions
     :return: torch.Tensor (..., (deg+1) ** 2)
     """
-    assert deg <= 4 and deg >= 0
+    assert 0 <= deg <= 4
     result = torch.empty((*dirs.shape[:-1], (deg + 1) ** 2), dtype=dirs.dtype, device=dirs.device)
     result[..., 0] = C0
     if deg > 0:
         x, y, z = dirs.unbind(-1)
-        result[..., 1] = -C1 * y;
-        result[..., 2] = C1 * z;
-        result[..., 3] = -C1 * x;
+        result[..., 1] = -C1 * y
+        result[..., 2] = C1 * z
+        result[..., 3] = -C1 * x
         if deg > 1:
             xx, yy, zz = x * x, y * y, z * z
             xy, yz, xz = x * y, y * z, x * z
-            result[..., 4] = C2[0] * xy;
-            result[..., 5] = C2[1] * yz;
-            result[..., 6] = C2[2] * (2.0 * zz - xx - yy);
-            result[..., 7] = C2[3] * xz;
-            result[..., 8] = C2[4] * (xx - yy);
-
+            result[..., 4] = C2[0] * xy
+            result[..., 5] = C2[1] * yz
+            result[..., 6] = C2[2] * (2.0 * zz - xx - yy)
+            result[..., 7] = C2[3] * xz
+            result[..., 8] = C2[4] * (xx - yy)
             if deg > 2:
-                result[..., 9] = C3[0] * y * (3 * xx - yy);
-                result[..., 10] = C3[1] * xy * z;
-                result[..., 11] = C3[2] * y * (4 * zz - xx - yy);
-                result[..., 12] = C3[3] * z * (2 * zz - 3 * xx - 3 * yy);
-                result[..., 13] = C3[4] * x * (4 * zz - xx - yy);
-                result[..., 14] = C3[5] * z * (xx - yy);
-                result[..., 15] = C3[6] * x * (xx - 3 * yy);
-
+                result[..., 9] = C3[0] * y * (3 * xx - yy)
+                result[..., 10] = C3[1] * xy * z
+                result[..., 11] = C3[2] * y * (4 * zz - xx - yy)
+                result[..., 12] = C3[3] * z * (2 * zz - 3 * xx - 3 * yy)
+                result[..., 13] = C3[4] * x * (4 * zz - xx - yy)
+                result[..., 14] = C3[5] * z * (xx - yy)
+                result[..., 15] = C3[6] * x * (xx - 3 * yy)
                 if deg > 3:
-                    result[..., 16] = C4[0] * xy * (xx - yy);
-                    result[..., 17] = C4[1] * yz * (3 * xx - yy);
-                    result[..., 18] = C4[2] * xy * (7 * zz - 1);
-                    result[..., 19] = C4[3] * yz * (7 * zz - 3);
-                    result[..., 20] = C4[4] * (zz * (35 * zz - 30) + 3);
-                    result[..., 21] = C4[5] * xz * (7 * zz - 3);
-                    result[..., 22] = C4[6] * (xx - yy) * (7 * zz - 1);
-                    result[..., 23] = C4[7] * xz * (xx - 3 * yy);
-                    result[..., 24] = C4[8] * (xx * (xx - 3 * yy) - yy * (3 * xx - yy));
+                    result[..., 16] = C4[0] * xy * (xx - yy)
+                    result[..., 17] = C4[1] * yz * (3 * xx - yy)
+                    result[..., 18] = C4[2] * xy * (7 * zz - 1)
+                    result[..., 19] = C4[3] * yz * (7 * zz - 3)
+                    result[..., 20] = C4[4] * (zz * (35 * zz - 30) + 3)
+                    result[..., 21] = C4[5] * xz * (7 * zz - 3)
+                    result[..., 22] = C4[6] * (xx - yy) * (7 * zz - 1)
+                    result[..., 23] = C4[7] * xz * (xx - 3 * yy)
+                    result[..., 24] = C4[8] * (xx * (xx - 3 * yy) - yy * (3 * xx - yy))
     return result
