9927f81aea80ceebd2e326443c0c6484fda12ec0
Wenri <Wenri@users.noreply.github.com>
2022-11-29 15:23:36 +0800

    semantic_helper

--- dataLoader/semantic_helper.py
+++ dataLoader/semantic_helper.py
@@ -11,11 +11,19 @@ class VGGSemantic(nn.Module):
         for layer in self.vgg.features:
             if isinstance(layer, nn.MaxPool2d):
                 layer.register_forward_pre_hook(self._call_back)
+        self.layer_features = {}
 
     def _call_back(self, layer, args):
-        print(args[0].shape)
+        x = args[0]
+        t = self.layer_features.setdefault(tuple(x.shape), x)
+        assert x is t
 
     def forward(self, x):
         x = self.preprocess(x).unsqueeze(0)
-        x = self.vgg.features(x)
+
+        self.layer_features.clear()
+        self.vgg.features(x)
+        x = list(self.layer_features.values())
+        self.layer_features.clear()
+
         return x
