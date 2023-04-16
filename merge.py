84d330feef4753328d9f675a1950b493734bf02c
Wenri <Wenri@users.noreply.github.com>
2023-04-16 20:31:45 +0800

    sub command parser

--- merge.py
+++ merge.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from datetime import datetime
 from pathlib import Path
 
 import numpy as np
@@ -18,9 +17,9 @@ class Merger:
 
         # init dataset
         dataset = dataset_dict[args.dataset_name]
-        self.train_dataset = dataset(args.datadir, split='train', downsample=args.downsample_train, is_stack=False,
+        self.train_dataset = dataset(args.datadir, split='train', downsample=args.downsample_test, is_stack=False,
                                      semantic_type=args.semantic_type)
-        self.test_dataset = dataset(args.datadir, split='test', downsample=args.downsample_train, is_stack=True,
+        self.test_dataset = dataset(args.datadir, split='test', downsample=args.downsample_test, is_stack=True,
                                     semantic_type=args.semantic_type, pca=getattr(self.train_dataset, 'pca', None))
 
         self.args = args
@@ -67,8 +66,6 @@ class Merger:
                 self.logger.warning('the ckpt path does not exists!!')
         else:
             logfolder = Path(args.basedir, args.expname)
-            if args.add_timestamp:
-                logfolder = logfolder / datetime.now().strftime("-%Y%m%d-%H%M%S")
 
         PSNRs_test = None
         if args.render_train:
@@ -95,9 +92,39 @@ class Merger:
         return PSNRs_test or PSNRs_path
 
 
-def main(args):
-    print(args)
+def config_parser(parser):
+    from dataLoader import dataset_dict
+    from models import MODEL_ZOO
 
+    parser.add_argument('--config', is_config_file=True, help='config file path')
+    parser.add_argument("--expname", type=str, help='experiment name')
+    parser.add_argument("--basedir", type=str, default='./log', help='where to store ckpts and logs')
+    parser.add_argument("--datadir", type=str, default='./data/llff/fern', help='input data directory')
+
+    parser.add_argument('--downsample_test', type=float, default=1.0)
+
+    parser.add_argument('--model_name', type=MODEL_ZOO.get, default='TensorVMSplit', choices=MODEL_ZOO)
+    parser.add_argument('--dataset_name', type=str, default='blender', choices=dataset_dict.keys())
+
+    # network decoder
+    parser.add_argument("--semantic_type", type=str, default='None', help='semantic type')
+
+    parser.add_argument("--ckpt", type=str, default=None,
+                        help='specific weights npy file to reload for coarse network')
+    parser.add_argument("--render_only", action="store_true")
+    parser.add_argument("--render_test", action="store_true")
+    parser.add_argument("--render_train", action="store_true")
+    parser.add_argument("--render_path", action="store_true")
+    parser.add_argument("--export_mesh", action="store_true")
+
+    # rendering options
+    parser.add_argument('--ndc_ray', type=int, default=0)
+
+    # logging/saving options
+    parser.add_argument("--N_vis", type=int, default=5, help='N images to vis')
+
+
+def main(args):
     merger = Merger(args)
     if args.export_mesh:
         merger.export_mesh()
